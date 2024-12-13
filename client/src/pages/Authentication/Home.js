bfd01c9fc21ad869a0ced5443a33936dcfef1f9a
Zahed Hossen <zahedsiddique10@gmail.com>
2024-12-13 15:42:58 +0600

    Remove CRA client folder

--- client/src/pages/Authentication/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-    return <h1>Welcome to SchoolBridge</h1>;
-};
-
-export default Home;
--- client/src/pages/UserRolePage/Dashboards.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const TeacherDashboard = () => <h1>Welcome to the Teacher Dashboard</h1>;
-const StudentDashboard = () => <h1>Welcome to the Student Dashboard</h1>;
-const ParentDashboard = () => <h1>Welcome to the Parent Dashboard</h1>;
-const AdminDashboard = () => <h1>Welcome to the Admin Dashboard</h1>;
-
-export { TeacherDashboard, StudentDashboard, ParentDashboard, AdminDashboard };
